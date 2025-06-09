600dcc60f81f8d357232dc55ad466d0111fa1ac7
Dylan Awalt-Conley <web@dylan.ac>
2025-06-09 12:09:38 -0400

    build: configure css inlining using esbuild-sass-plugin

---
+++ typings/esbuild-plugin-sass.d.ts
@@ -0,0 +1,14 @@
+declare module '*.css' {
+  const cssText: string;
+  export = cssText;
+}
+
+declare module '*.scss' {
+  const cssText: string;
+  export = cssText;
+}
+
+declare module '*.sass' {
+  const cssText: string;
+  export = cssText;
+}
